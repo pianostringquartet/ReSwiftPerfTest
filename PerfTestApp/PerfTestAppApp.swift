f26219e375e49a08a22d4f78c86d7f24d7bde703
Chris Clampitt <christian.clampitt@nyu.edu>
2022-11-18 13:59:21 -0700

    Initial Commit

---
+++ PerfTestApp/PerfTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfTestAppApp.swift
+//  PerfTestApp
+//
+//  Created by Christian J Clampitt on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct PerfTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
