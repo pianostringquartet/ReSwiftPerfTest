0c8612fe8902a685ab7ca2238ec73b0f95a22d7b
Chris Clampitt <christian.clampitt@nyu.edu>
2022-11-23 16:50:08 -0700

    Experiments

--- PerfTestApp/PerfTestAppApp.swift
+++ PerfTestApp/PerfTestAppApp.swift
@@ -6,12 +6,49 @@
 //
 
 import SwiftUI
+import DisplayLink
+
+
+/*
+ Holding ReSwift store in SwiftUI View vs Scene/App:
+ 
+ Store, onFrame in View: 16-17% CPU
+
+ Store, onFrame in App: 32-33% CPU
+ 
+ Store in App, onFrame + dispatch in View: 8-9% CPU
+ */
 
 @main
 struct PerfTestAppApp: App {
+    
+    @StateObject var storeData = ReSwiftStore<ReSwiftState>(store: reswiftStore())
+    
     var body: some Scene {
         WindowGroup {
-           ReSwiftPerfPlay()
+//           ReSwiftPerfPlay()
+            
+//            PerfTestView()
+            PerfTestView(dispatch: storeData.dispatch)
+//                .onFrame { _ in
+//                    storeData.dispatch(CounterIncremented())
+//                }
         }
+    }
+}
+
+
+struct PerfTestView: View {
+    
+//    @StateObject var storeData = ReSwiftStore<ReSwiftState>(store: reswiftStore())
+
+    let dispatch: Dispatch
+    
+    var body: some View {
+        Text("UI does not use state")
+            .onFrame { _ in
+//                storeData.dispatch(CounterIncremented())
+                dispatch(CounterIncremented())
+            }
     }
 }
